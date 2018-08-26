24cfbc868e017f8b9bcc798bad89de6236e09f6a
Mateja Marjanovic <mi15712@alas.matf.bg.ac.rs>
2018-08-26 20:17:23 +0200

    A bit adjusted classification, the error is greater but it has more sense.

--- klasifikacija/sumDeathsAndInjuries.py
+++ klasifikacija/sumDeathsAndInjuries.py
@@ -1,44 +1,37 @@
 import pandas
 
 def changeDateFormat(df):
-    for i, row in df.iterrows():
-        month = row["Incident Date"].strip().split(' ')[0].split(',')[0]
-        df.set_value(i, "Incident Date", month)
+    df = df.drop(columns = ["Unnamed: 0", "Incident Date"])
     
-    df = df.drop(columns = ["Unnamed: 0"])
-    
-    injuriesMonths = {}
+    incidents = {}
     for i, row in df.iterrows():
-        month = row["Incident Date"].strip()
         state = row["State"].strip()
         city = row["City Or County"].strip()
-        if month + ":" + state not in injuriesMonths:
-            injuriesMonths[month + ":" + state + ":" + city] = int(row["# Injured"]) + int(row["# Killed"])
+        if state + ":" + city not in incidents:
+            incidents[state + ":" + city] = int(row["# Injured"]) + int(row["# Killed"])
         else:
-            injuriesMonths[month + ":" + state + ":" + city] += int(row["# Injured"]) + int(row["# Killed"])
+            incidents[state + ":" + city] += int(row["# Injured"]) + int(row["# Killed"])
         df = df.drop([i])
-        #print i
     
     df = df.drop(columns = ["# Killed", "# Injured"])
-    for k, v in injuriesMonths.iteritems():
+    for k, v in incidents.iteritems():
         key = k.split(':')
-        city = key[2]
-        state = key[1]
-        month = key[0]
+        city = key[1]
+        state = key[0]
         numberOfDeaths = v
-        df = df.append({"Incident Date" : month, "State" : state, "City Or County" : city, "# Incidents" : int(numberOfDeaths)}, ignore_index = True)
+        df = df.append({"State" : state, "City Or County" : city, "# Incidents" : int(numberOfDeaths)}, ignore_index = True)
     
     return df
 
 def main():
-    df = pandas.read_csv("processed_injuries.csv")
-    df2 = pandas.read_csv("processed_deaths.csv")
+    df = pandas.read_csv("../processed_injuries.csv")
+    df2 = pandas.read_csv("../processed_deaths.csv")
     for i, row in df2.iterrows():
         df = df.append(row, ignore_index = True)
     
     df = changeDateFormat(df)
     
-    with open("processed_changed_date_injuries.csv", "w") as csv_file:
+    with open("summedIncidentsPerCityAndState.csv", "w") as csv_file:
         csv = df.to_csv(index = False)
         csv_file.write(csv)
 
